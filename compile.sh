dc51f6aa388451462f1f6f8808f46a3214a6a9c9
Rafael Santos Silva <rafael@MacBook-Pro-M3-de-Rafael.local>
2024-06-22 23:17:15 -0300

    (feature) #17 build

--- compile.sh
+++ compile.sh
@@ -16,3 +16,22 @@ npm i
 cd ..
 cd tea-care-bff-ms-auth
 npm i
+cd ..
+cd tea-care-bfb-ms-common
+npm run build
+cd ..
+cd tea-care-bff-ms-config
+npm run build
+cd ..
+cd tea-care-bff-ms-therapeutic-activity
+npm run build
+cd ..
+cd tea-care-bff-ms-dashboard
+npm run build
+cd ..
+cd tea-care-bff-ms-report
+npm run build
+cd ..
+cd tea-care-bff-ms-auth
+npm run build
+
