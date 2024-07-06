c5c6ff0e63f5739b38fae32a7e9eefd752bb30fb
Rafael Santos Silva <rafael.santos@pitang.com>
2024-07-06 14:17:02 -0300

    #110 Ajuste

--- tea-care-bff-ms-config/src/routes/user/new.ts
+++ tea-care-bff-ms-config/src/routes/user/new.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express';
 import sanitizeHtml from 'sanitize-html';
+const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt');
 
 import {
+  ProfileDoc,
+  ProfileSchema,
+  generatePassword,
   getTenantByOrigin,
+  logger,
   mongoWrapper,
   sanitizeArray,
 } from '@teacare/tea-care-bfb-ms-common';
@@ -14,6 +20,7 @@ import {
   UserDoc,
 } from '@teacare/tea-care-bfb-ms-common';
 import { userValidations } from '../../middlewares/userValidations';
+import { OccupationType } from '@teacare/tea-care-bfb-ms-common/build/src/models/care-type';
 
 const router = express.Router();
 
@@ -31,7 +38,8 @@ router.put(
       const email = sanitizeHtml(req.body.email) as string;
       const phone = sanitizeHtml(req.body.phone) as string;
       const type = sanitizeHtml(req.body.type) as string;
-      const profiles = sanitizeArray(req.body.profiles);
+      const occupation = sanitizeHtml(req.body.occupation) as OccupationType;
+      const profilesSymbol = sanitizeArray(req.body.profiles) as Array<string>;
 
       const tenant: string = getTenantByOrigin(req);
 
@@ -50,12 +58,59 @@ router.put(
         throw new BadRequestError('Usuário já existe.');
       }
 
+      //gerar senha
+      const newPassword = generatePassword();
+      const salt = await bcrypt.genSalt(12);
+      const passwordHash = await bcrypt.hash(newPassword, salt);
+
+      const profiles = await mongoWrapper
+        .getModel<ProfileDoc>(tenant, 'Profile', ProfileSchema)
+        .find({
+          symbol: {
+            $in: [profilesSymbol],
+          },
+        });
+
       const user = new User({
         name: name,
         cpf: cpf,
         phone: phone,
         email: email,
         type: type,
+        occupation: occupation,
+        password: passwordHash,
+        profiles: profiles,
+      });
+
+      const transport = nodemailer.createTransport({
+        service: 'Gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.MAIL_USERNAME,
+          pass: process.env.MAIL_PASSWORD,
+        },
+      });
+
+      const message = {
+        from: `Tea Care <${process.env.MAIL_USERNAME}>`,
+        to: email,
+        subject: 'Bem-vindo ao Tea Care',
+        html: `<h2>Olá ${name}!</h2> <p> Senha de acesso ao Tea Care: ${newPassword} </p>`,
+        text: `Olá ${name}! Senha de acesso ao Tea Care: ${newPassword}`,
+      };
+
+      transport.sendMail(message, (error: any, info: any) => {
+        if (error) {
+          logger.info(
+            `Erro ao enviar e-mail de boas vindas. Mensagem ${error}`
+          );
+        } else {
+          logger.info(
+            `Mensagem de boas vindas enviada com sucesso. ${info.response}`
+          );
+        }
       });
 
       await user.save();
---
+++ tea-care-bff-ms-config/src/routes/user/users.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response, NextFunction } from 'express';
+import {
+  mongoWrapper,
+  validateRequest,
+  UserSchema,
+  UserDoc,
+  getTenantByOrigin,
+} from '@teacare/tea-care-bfb-ms-common';
+
+const router = express.Router();
+
+/**
+ * Listagem para associação
+ */
+router.get(
+  '/api/users',
+  validateRequest,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const tenant: string = getTenantByOrigin(req);
+
+      const User = await mongoWrapper.getModel<UserDoc>(
+        tenant,
+        'User',
+        UserSchema
+      );
+
+      const users = await User.aggregate([
+        {
+          $project: { cpf: 1, name: 1, _id: 1 },
+        },
+        { $sort: { name: 1 } },
+      ]);
+
+      res.send(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export { router as listUserRouter };
